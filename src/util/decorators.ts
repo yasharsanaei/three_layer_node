d4e4f797b5ad2d9920bf9a9a980b9a4b303e1e5e
yashar <yasharsanayee@gmail.com>
2022-01-27 00:44:52 +0330

    ready to create paths

--- src/util/decorators.ts
+++ src/util/decorators.ts
@@ -13,3 +13,77 @@ export function PageRoute(path: string) {
         return descriptor;
     };
 }
+
+export function RequestMapping(path: string) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        Reflect.defineMetadata(
+            propertyKey,
+            {
+                ...Reflect.getMetadata(propertyKey, target),
+                baseRoute: path,
+            },
+            target,
+        );
+        return descriptor;
+    };
+}
+
+export function GetMapping(path: string) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        Reflect.defineMetadata(
+            propertyKey,
+            {
+                ...Reflect.getMetadata(propertyKey, target),
+                route: path,
+                type: 'get',
+            },
+            target,
+        );
+        return descriptor;
+    };
+}
+
+export function PostMapping(path: string) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        Reflect.defineMetadata(
+            propertyKey,
+            {
+                ...Reflect.getMetadata(propertyKey, target),
+                route: path,
+                type: 'post',
+            },
+            target,
+        );
+        return descriptor;
+    };
+}
+
+export function PutMapping(path: string) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        Reflect.defineMetadata(
+            propertyKey,
+            {
+                ...Reflect.getMetadata(propertyKey, target),
+                route: path,
+                type: 'put',
+            },
+            target,
+        );
+        return descriptor;
+    };
+}
+
+export function DeleteMapping(path: string) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        Reflect.defineMetadata(
+            propertyKey,
+            {
+                ...Reflect.getMetadata(propertyKey, target),
+                route: path,
+                type: 'delete',
+            },
+            target,
+        );
+        return descriptor;
+    };
+}
